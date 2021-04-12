534e5464b0b6f666396b7f32869ce3b162ec82b2
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-04-12 08:15:12 -0700

    add rocket

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -15,7 +15,9 @@ class Play extends Phaser.Scene {
         this.add.rectangle(0, 0, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, game.config.height - borderUISize, game.config.width, borderUISize, 0xFFFFFF).setOrigin(0, 0);
         this.add.rectangle(0, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);
-        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);        
+        this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);      
+        // rocket
+        this.rocket = new Rocket(this, game.config.width / 2, game.config.height - borderUISize, 'rocket').setOrigin(0.5, 0);  
     }
 
     update(){
