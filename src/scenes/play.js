fb148070da9a2804528fa317404065d5b14d5f44
dninemfive <11084010+dninemfive@users.noreply.github.com>
2021-04-12 08:23:23 -0700

    rocket controls

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -18,9 +18,15 @@ class Play extends Phaser.Scene {
         this.add.rectangle(game.config.width - borderUISize, 0, borderUISize, game.config.height, 0xFFFFFF).setOrigin(0, 0);      
         // rocket
         this.rocket = new Rocket(this, game.config.width / 2, game.config.height - borderUISize, 'rocket').setOrigin(0.5, 0);  
+        
+        keyF = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        keyR = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.R);
+        keyLEFT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT);
+        keyRIGHT = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT);
     }
 
     update(){
         this.starfield.tilePositionX -= 4;
+        this.rocket.update();
     }
 }
